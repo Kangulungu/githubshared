de116d6f1b8bc0a616fbf3ab5c4935c17e0a8a25
cybbe <cybbe@LAPTOP-3UL0ED8C>
2020-11-30 17:19:48 +0100

    Maintenance Client

---
+++ csharp/storeDemo/StoreDemoWebApi/AngularCLient/src/app/robot-details/robot-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-robot-details',
+  templateUrl: './robot-details.component.html',
+  styles: [
+  ]
+})
+export class RobotDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
